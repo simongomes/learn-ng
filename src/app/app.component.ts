09348d276dfefa6e4fcaf4fd41b41991be08939c
SIMON GOMES <busy.s.simon@gmail.com>
2024-10-03 18:46:42 +0800

    feat: home and housing location component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import {HomeComponent} from './home/home.component';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   // imports: [RouterOutlet],
-  // templateUrl: './app.component.html',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  template: `<h1>Hello World! {{title}}</h1>`
+  imports: [
+    HomeComponent
+  ]
 })
 export class AppComponent {
   title = 'play-ng';
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocationComponent} from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
