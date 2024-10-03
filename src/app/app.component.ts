7f16f23577d499aeac2f16f035cced7436d37080
SIMON GOMES <busy.s.simon@gmail.com>
2024-10-03 18:25:01 +0800

    chore: ng app initialized for learning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  // imports: [RouterOutlet],
+  // templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  template: `<h1>Hello World! {{title}}</h1>`
 })
 export class AppComponent {
   title = 'play-ng';
